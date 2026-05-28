f5daac32d59381fbe77f8a8fc60122e1a60767a1
promise <kamangapromise228@gmail.com>
2026-05-28 15:41:26 +0200

    added routes to develop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Cart } from './cart/cart';
+import { Home } from './home/home';
+import { Shop } from './shop/shop';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path:'about',component: About},
+    {path:'cart',component: Cart},
+    {path:'home',component: Home},
+    {path:'shop',component: Shop},
+];
