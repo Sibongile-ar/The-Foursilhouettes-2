1e018cbf66c4214eefe5383316bf3f48b49700bb
Charlotte <cnkuna920@gmail.com>
2026-05-28 16:21:05 +0200

    added product service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Home } from './home/home';
 import { Shop } from './shop/shop';
 
 export const routes: Routes = [
-     {path:'about',component: About},
+    {path:'about',component: About},
     {path:'cart',component: Cart},
     {path:'home',component: Home},
     {path:'shop',component: Shop},
