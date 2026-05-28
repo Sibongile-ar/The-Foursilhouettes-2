4191db4ec3e709c44aa14e48fe009ce3bf11c7b2
promise <kamangapromise228@gmail.com>
2026-05-28 15:49:17 +0200

    routes added to develop

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, RouterOutlet, Footer,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
