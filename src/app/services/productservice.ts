79b62a181077d684dc3f892f06a1fcf7b2b5f238
promise <kamangapromise228@gmail.com>
2026-05-29 09:40:40 +0200

    added navbar protcols

--- src/app/services/productservice.ts
+++ src/app/services/productservice.ts
@@ -67,7 +67,7 @@ export class Productservice {
       "price": 440.99,
       "description": "Luxurious black silk satin slip dress and robe set. Ultra-soft satin fabric drapes elegantly over the body with lace trim accents for a touch of romance.",
       "category": "lingerie",
-      "image": "/BlackSilk Set.webp",
+      "image": "/Blacksilkset.webp",
     },
     {
       "id": 9,
