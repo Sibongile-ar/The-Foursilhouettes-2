e624124d67ee1ac919149dbb968f00d264a59ac2
Sibongile <sibongilearathokwane@gmail.com>
2026-06-02 10:32:17 +0200

    was fixing my branch

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Cartservice } from '../services/cartservice';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-    constructor(public cartService: Cartservice) {}
+    constructor(
+      public cartService: Cartservice,
+      public authService: AuthService
+    ) {}
 
-
+    logout() {
+      this.authService.logout();
+    }
 }
