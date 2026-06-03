61f96373c78a0afcb36ded306568789402b12113
Sibongile <sibongilearathokwane@gmail.com>
2026-06-03 13:45:08 +0200

    fixed logon/register

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -48,8 +48,11 @@ export class Home {
       quantity: 1
     };
 
-    this.cartService.addToCart(cartProduct);
-
-    this.toastr.success('Added to Cart');
+    const added = this.cartService.addToCart(cartProduct);
+    if (added) {
+      this.toastr.success('Added to Cart');
+    } else {
+      this.toastr.error('Please login to add items to your cart');
+    }
   }
 }
